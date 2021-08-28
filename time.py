b549947dae225073e25dc9f0f59730db72059358
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-28 14:58:41 +0430

    Add files via upload

---
+++ time.py
@@ -0,0 +1,97 @@
+class hour():
+    
+    def __init__(self, hh, mm, ss):
+        self.hour = hh
+        self.minute = mm
+        self.second = ss
+
+        self.hour1 = hh
+        self.minute1 = mm
+        self.second1 = ss
+        
+    def sum(self):
+        time ={}
+        
+        time.second = self.second + self.second1
+        time.minute = self.minute + self.minute1minute
+        time.hour = self.hour + self.hour1
+        
+        if time.second >= 60:
+            time.second -= 60
+            time.minute += 1
+        
+        if time.minute >= 60:
+            time.minute -= 60
+            time.hour += 1
+            
+        return time
+    
+    def sub(self, mehman):
+        time = {}
+        
+        if self.second >= self.second1:
+            time.second = self.second - self.second1
+        else:
+            self.second += 60
+            self.minute -= 1
+            time.second = self.second - self.second1
+        
+        if self.minute >= self.minute1:
+           time.minute = self.minute - self.minute1
+        else:
+            self.minute += 60
+            self.hour -= 1
+            time.minute = self.minute - self.minute1
+        
+        time.hour = self.hour - self.hour1
+        if time.hour < 0:
+            time.hour += 24
+        
+        return time
+    
+    def hour_to_sec(self):
+        time = {}
+        time.hour = 0
+        time.minute = 0
+        time.second = self.second + self.minute * 60 + self.hour * 3600
+        return time
+    
+    def sec_to_hour(self):
+        time = {}
+        
+        time.hour = self.second // 3600
+        self.second %= 3600
+
+        time.minute = self.second // 60
+        self.second %= 60
+
+        time.second = self.second
+        
+        return time
+    
+    def show(self):
+        print(self.hour, ':', self.minute, ':', self.second)
+
+h_1 = int(input('Hour(1) = '))
+m_1 = int(input('Minute(1) = '))
+s_1 = int(input('Second(1) = '))
+
+h_2 = int(input('Hour(2) = '))
+m_2 = int(input('Minute(2) = '))
+s_2 = int(input('Second(2) = '))
+
+    
+a = time(h_1, m_1, s_1)
+b = time(h_2, m_2, s_2)
+
+c1 = a.sum(b)
+c1.show()
+
+c2 = a.sub(b)
+c2.show()
+
+c3 = a.timetosec()
+c3.show()
+
+c4 = c3.sectotime()
+c4.show()
